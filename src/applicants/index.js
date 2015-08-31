1db7f51a0276af29c575c7e66c3a33be99a4a5f3
ChrisMcKenzie <chris@kbsurfer.com>
2015-08-31 11:05:35 -0700

    store dob properly in db, display it properly too

--- src/applicants/index.js
+++ src/applicants/index.js
@@ -1,10 +1,13 @@
-import r from 'rethinkdb';
-import debug from 'debug';
 import Collection from '../collection';
 
 export default class Applicants extends Collection {
   constructor() {
     // Call parent class and tell it what table we are using
     super('applicants');
+  }
+
+  insert(applicant) {
+    applicant.dob = new Date(applicant.dob);
+    return super.insert(applicant);
   }
 }
